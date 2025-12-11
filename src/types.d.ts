575da0b2552820a5eb807911fe93d3a1fb6f4b6b
Arman karimi <arman.ui.dev@gmail.com>
2025-12-12 00:25:29 +0330

    feat: types for WildRiftAPI + related models
    Introduces TypeScript types and interfaces for abilities, skins, champions, and the WildRiftAPI. These definitions provide structure for champion data and API methods, improving type safety and code clarity.

---
+++ src/types.d.ts
@@ -0,0 +1,46 @@
+type AbilityType =
+	| 'passive'
+	| 'first'
+	| 'second'
+	| 'third'
+	| 'ultimate';
+
+interface Ability {
+	name: string;
+	video_url: string;
+	description: string;
+	ability_type: AbilityType;
+}
+interface Skin {
+	name: string;
+	image_url: string;
+}
+declare enum ChampionDifficulty {
+	Easy = 1,
+	Medium,
+	Hard
+}
+
+interface ChampionHeader {
+	id: string;
+	name: string;
+	image_url: string;
+}
+
+interface Champion extends ChampionHeader {
+	role: string;
+	skins: Skin[];
+	difficulty: ChampionDifficulty;
+	abilities: Record<AbilityType, Ability>;
+}
+
+export interface WildRiftAPI {
+	readonly championList: Champion[];
+	readonly championHeaders: ChampionHeader[];
+	/** Fetch all champion headers (basic info); stored in championHeaders	*/
+	loadChampionHeaders(): Promise<ChampionHeader[]>;
+	/** Returns a champion. Does not automatically store in class */
+	fetchChampion(champion: ChampionHeader): Promise<Champion>;
+	/** Fetch all champions; stored in championList */
+	loadChampions(): Promise<Champion[]>;
+}
