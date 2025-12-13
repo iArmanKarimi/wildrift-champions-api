83eb228bad301a7218c71930b07e138f54632062
Arman karimi <arman.ui.dev@gmail.com>
2025-12-13 23:56:07 +0330

    chore: Switch build output to ESM only and update exports
    Removed CommonJS build output and related exports from package.json and tsup config. The package now only outputs ESM format, simplifying distribution and usage. Version bumped to 0.1.2 to reflect this breaking change.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+	outDir: "dist",
 	entry: ['src/index.ts'],
-	format: ['cjs', 'esm'],
+	format: ['esm'],
 	dts: true,
 	sourcemap: true,
 	clean: true,
